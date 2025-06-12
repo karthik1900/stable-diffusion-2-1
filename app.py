f6162ad491cca425d67419bd85d5eddb2cb6dcbd
Karthik Sunkishala <40236661+karthik1900@users.noreply.github.com>
2025-06-12 15:54:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,35 @@
-import time
 print("time1: ", time.time())
-import os
-os.environ["HF_HUB_ENABLE_HF_TRANSFER"]='1'
+from diffusers import StableDiffusionPipeline
 print("time2: ", time.time())
-from huggingface_hub import snapshot_download
-print("time3: ", time.time())
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
-print("time4: ", time.time())
 import torch
-print("time5: ", time.time())
+print("time3: ", time.time())
 from io import BytesIO
-print("time6: ", time.time())
 import base64
-print("time7: ", time.time())
+import os
+print("time4: ", time.time())
 
 class InferlessPythonModel:
     def initialize(self):
-        print("time8: ", time.time())
-        model_id = "stabilityai/stable-diffusion-2-1"
-        snapshot_download(repo_id=model_id,allow_patterns=["*.safetensors"])
-        print("time9: ", time.time())
-        self.pipe = StableDiffusionPipeline.from_pretrained(model_id,use_safetensors=True,torch_dtype=torch.float16).to("cuda")
-        print("time10: ", time.time())
-        self.pipe.scheduler = DPMSolverMultistepScheduler.from_config(self.pipe.scheduler.config)
-        print("time11: ", time.time())
-        
+        print("time5: ", time.time())
+        self.pipe = StableDiffusionPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-2-1",
+            use_safetensors=True,
+            torch_dtype=torch.float16,
+            device_map='balanced'
+        )
+        print("time6: ", time.time())
+
+
     def infer(self, inputs):
-        print("time12: ", time.time())
+        print("time7: ", time.time())
         prompt = inputs["prompt"]
-        image = self.pipe(prompt,negative_prompt="low quality",num_inference_steps=9).images[0]
-        print("time13: ", time.time())
+        image = self.pipe(prompt).images[0]
+        print("time8: ", time.time())
         buff = BytesIO()
         image.save(buff, format="JPEG")
         img_str = base64.b64encode(buff.getvalue()).decode()
-        print("time14: ", time.time())
+        print("time9: ", time.time())
+        return { "generated_image_base64" : img_str }
         
-        return {"generated_image_base64" : img_str }
-    
     def finalize(self):
         self.pipe = None
